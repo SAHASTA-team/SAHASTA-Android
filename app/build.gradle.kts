b1194d0359c9db10aa52506fe56a8fa373fd1d5f
SAHASTA-team <sahastateam@gmail.com>
2024-12-01 21:51:33 +0700

    feat: layout scan nutrition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,6 +47,11 @@ dependencies {
     implementation(libs.fragment.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.camera.core)
+    implementation(libs.camera.camera2)
+    implementation(libs.camera.view)
+    implementation(libs.camera.lifecycle)
+    implementation(libs.camera.extensions)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/salma/sahasta/presentation/main/MainActivity.kt
+++ app/src/main/java/com/salma/sahasta/presentation/main/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.salma.sahasta.presentation.main
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -18,13 +19,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+        window.statusBarColor = Color.WHITE
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
         setOnclickAction()
         setupBottomNav()
     }
@@ -44,6 +46,8 @@ class MainActivity : AppCompatActivity() {
     private fun navigateToNutrition() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         binding.navView.selectedItemId = R.id.menu_tab_nutrition
-        navController.navigate(R.id.menu_tab_nutrition)
+        if (navController.currentDestination?.id != R.id.menu_tab_nutrition) {
+            navController.popBackStack(R.id.menu_tab_nutrition, false)
+        }
     }
 }
